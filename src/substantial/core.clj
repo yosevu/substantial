83193f46856b661519034526cf6a7778757d1b3d
Yosevu Kilonzo <yosevuk@gmail.com>
2022-06-12 20:54:40 +0900

    feat: write notes pages to /public

--- src/substantial/core.clj
+++ src/substantial/core.clj
@@ -5,18 +5,26 @@
 (defn note-pages
   "Create pages for notes."
   [notes]
-  (for [[_ note] notes]
-    (note-page note)))
+  (for [[page-name note] notes]
+    [page-name (note-page note)]))
+
+(defn write-page [[page-name content]]
+  (spit (str "public/" (name page-name) ".html") content))
+
+(defn write-pages [notes]
+  (map write-page (note-pages notes)))
 
 (defn -main
   "Write note pages to files."
   []
+  (write-pages (get-notes "notes"))
   (println "Called (-main)"))
 
 (comment
-  (get-notes "notes")
-  (for [[_ note] (get-notes "notes")]
-    note)
-  (note-pages (get-notes "notes"))
-  ;; (map note-page (get-notes "notes"))
-  (-main))
+  ;; (get-notes "notes")
+  ;; (for [[_ note] (get-notes "notes")] note)
+  ;; (let [notes (get-notes "notes")] notes)
+  ;; (note-pages (get-notes "notes"))
+  ;; (map write-page (note-pages (get-notes "notes")))
+  (-main)
+  )
