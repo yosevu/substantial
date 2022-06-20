86cb15824e3f47ebaedfd8bad63cffdd8667fa1b
Yosevu Kilonzo <yosevuk@gmail.com>
2022-06-20 21:14:36 +0900

    chore: Update publish script

--- src/substantial/scripts.clj
+++ src/substantial/scripts.clj
@@ -2,13 +2,15 @@
   (:require [clojure.java.shell :refer [sh]]
             [clojure.string :refer [split trim]]))
 
-(defn git-subtree [] (trim (:out (apply sh (split "git subtree split --prefix public main" #" ")))))
+(defn git-subtree []
+  (trim (:out (apply sh (split "git subtree split --prefix public main" #" ")))))
   
 (defn git-push []
   (let [commit-sha (git-subtree)]
     (apply sh (split (str "git push origin " commit-sha ":gh-pages --force") #" "))))
   
 (defn publish [_]
+  (println "Publishing site...")
   (let [{:keys [out err]} (git-push)]
     (when (seq out) (print out))
     (when (seq err) (print err))))
