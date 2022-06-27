c136238892759bfb5beffcdfe35734870fd29609
Yosevu Kilonzo <yosevuk@gmail.com>
2022-06-27 15:34:14 +0900

    refactor: Refactor utilities

--- src/substantial/utilities.clj
+++ src/substantial/utilities.clj
@@ -2,25 +2,25 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-;; TODO use spec for this?
-(defn valid-filename [file]
-  (re-matches #"[a-z0-9|-]+(.md)" (.getName file)))
+(defn valid-filename
+  "Matches a markdown file with an alphanumeric filename."
+  [file]
+  (re-matches #"[a-z0-9|-]+.md" (.getName file)))
 
-(defn get-file [file]
+(defn get-file
+  "Gets a valid file or throws."
+  [file]
   (if (valid-filename file)
     (slurp file)
     (throw (Exception. (str "Error: " (.getName file) " is not a valid filename.")))))
 
 (defn get-files
-  "Gets a list of files from `path`."
+  "Gets a list of valid files from `path`."
   [path]
-  (map get-file (rest (file-seq (io/file path)))))
+  (map get-file (->> path io/file file-seq rest)))
 
 (defn get-config []
   (edn/read-string (slurp "config.edn")))
 
 (comment
-  (get-files "notes")
-  (re-matches #"[a-z0-9|-]+(.md)" (.getName (second (file-seq (io/file "notes")))))
-  (println (count (get-files "notes")))
-  (println (:site-title (get-config))))
+  (get-files "notes"))
