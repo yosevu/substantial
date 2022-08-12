d0c6d770b0ab6739e60dabde0c882f6e5de0fbcd
Yosevu Kilonzo <yosevuk@gmail.com>
2022-08-12 17:15:57 +0900

    refactor(publish): make asset path dynamic

--- src/substantial/scripts.clj
+++ src/substantial/scripts.clj
@@ -3,20 +3,22 @@
   (:require [clojure.java.shell :refer [sh]]
             [clojure.string :refer [split trim]]))
 
-(defn- git-subtree []
-  (trim (:out (apply sh (split "git subtree split --prefix static main" #" ")))))
+(defn- git-subtree [path]
+  (trim (:out (apply sh (split (str "git subtree split --prefix " path " main") #" ")))))
 
-(defn- git-push []
-  (let [commit-sha (git-subtree)]
+(defn- git-push [path]
+  (let [commit-sha (git-subtree path)]
     (apply sh (split (str "git push origin " commit-sha ":gh-pages --force") #" "))))
 
 (defn update [_]
   (println "Not implemented."))
 
-(defn publish [_]
+(defn publish
+  "Requires remote named gh-pages."
+  [_]
   (println (:out (sh "clj" "-M:build")))
   (println "Publishing site.")
-  (let [{:keys [out err]} (git-push)]
+  (let [{:keys [out err]} (git-push "static")]
     (when (seq out) (print out))
     (when (seq err) (print err)))
   (println "Published site."))
